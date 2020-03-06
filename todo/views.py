8a11ec638d473271cc5c0df05d6b071fe0d3683b
Neil_S <neil.smyth1989@hotmail.com>
2020-03-06 16:16:43 +0000

    first commit

---
+++ todo/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render, HttpResponse
+
+# Create your views here. View function
+"""
+def say_hello(request):
+    return HttpResponse('Hello World')
+"""
+
+def get_todo_list(request):
+    return render(request, "todo_list.html")
